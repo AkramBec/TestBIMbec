e251a851c62bc960ace70b9f0051dcd0cef4ea5f
AkramBec <akram.bec.96@gmail.com>
2025-06-21 19:30:51 +0300

    Исправление мелких ошибок

--- SearchFittingsInParkingSpaces/Models/RebarOverParkingAnalyzer.cs
+++ SearchFittingsInParkingSpaces/Models/RebarOverParkingAnalyzer.cs
@@ -27,9 +27,7 @@ public class RebarOverParkingAnalyzer
             .Cast<RevitLinkInstance>();
         
         // Арматура труб и воздуховодов в связях
-        
-        View3D fallbackViewToActivate = null;
-        
+
         using (var txDelete = new Transaction(doc, "Удаление старых видов"))
         {
             txDelete.Start();
@@ -103,4 +101,52 @@ public class RebarOverParkingAnalyzer
             doc.Delete(viewsToDelete);
         }
     }
+    
+    public static View3D SwitchToSafeViewIfNeeded(UIApplication uiApp, ViewFamilyType pluginViewType)
+        {
+            var uiDoc = uiApp.ActiveUIDocument;
+            var doc = uiDoc.Document;
+            var activeView = uiDoc.ActiveView as View3D;
+
+            // Если активный вид имеет тип, совпадающий с плагинным — заменяем
+            if (activeView.GetTypeId() == pluginViewType.Id)
+            {
+                // Ищем существующий вид, начинающийся с {3D
+                var safeView = new FilteredElementCollector(doc)
+                    .OfClass(typeof(View3D))
+                    .Cast<View3D>()
+                    .FirstOrDefault(v => !v.IsTemplate &&
+                                         v.Name.Contains("3D", StringComparison.OrdinalIgnoreCase) &&
+                                         v.GetTypeId() != pluginViewType.Id);
+
+                if (safeView != null)
+                {
+                    return safeView;
+                }
+    
+                // Найти ViewFamilyType для обычного 3D-вида, исключая pluginViewType
+                var safeViewType = new FilteredElementCollector(doc)
+                    .OfClass(typeof(ViewFamilyType))
+                    .Cast<ViewFamilyType>()
+                    .FirstOrDefault(vft =>
+                        vft.ViewFamily == ViewFamily.ThreeDimensional &&
+                        vft.Id != pluginViewType.Id);
+
+                // Если ни один не подошёл — создаём новый стандартный 3D вид
+                if (safeViewType != null)
+                {
+                    using (var tx = new Transaction(doc, "Создание безопасного вида"))
+                    {
+                        tx.Start();
+                        var default3DView = View3D.CreateIsometric(doc, safeViewType.Id);
+                        default3DView.Name = "3D";
+                        tx.Commit();
+                        
+                        return default3DView;
+                    }
+                    
+                }
+            }
+            return activeView;
+        }
 }
